b6e972bc7a511d2e633d73a1a4984c79cf7fbccf
Your Name <you@example.com>
2022-08-05 10:34:13 -0400

    Test

--- base/makeTestDockerfile.js
+++ base/makeTestDockerfile.js
@@ -10,7 +10,7 @@ ENV CI true
 
 RUN yarn init -y && yarn create playwright --quiet --lang=js && yarn playwright test
 `;
-    await fs.writeFile('/tmp/Dockerfile', content);
+    await fs.writeFile('/tmp/testdocker/Dockerfile', content);
     } catch (err) {
     throw err;
     }
