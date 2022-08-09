6a2702fe22fb221887ff9508b3fecb9ccb092f7c
Your Name <you@example.com>
2022-08-09 10:54:06 -0400

    test

--- base/build/puppeteerLaunch.js
+++ base/build/puppeteerLaunch.js
@@ -1,7 +1,15 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({'headless': true});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+    ]
+});
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({path: 'example.png'});
